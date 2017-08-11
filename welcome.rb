4aa5440851109c09d39e446221240d498ae77bb7
imidsac <imidsac@hotmail.fr>
2017-08-11 22:04:46 +0000

    Public and admin welcome page

--- welcome.rb
+++ welcome.rb
@@ -1,11 +1,8 @@
-generate :controller, "welcome index"
-route "root to: 'welcome#index'"
+################################### PUBLIC WELCOME ########################################
+if yes?("Do you want to create welcome page for public? [yes/no]")
 
-if yes?("Do you want route for /:inconnu? (yes/no)")
-  File.write("config/routes.rb", File.open("config/routes.rb", &:read).gsub("end", "
-  get '/:inconu' => 'welcome#inconu'
-end
-"))
+  generate :controller, "welcome home"
+  route "root to: 'welcome#home'"
 
   file 'app/controllers/welcome_controller.rb', <<-CODE
 class WelcomeController < ApplicationController
@@ -19,13 +16,51 @@ class WelcomeController < ApplicationController
 end
   CODE
 
+  file 'app/views/welcome/home.html.erb', <<-CODE
+<h1>Welcome to #{app_name.capitalize}!</h1>
+  CODE
+
+    File.write("config/routes.rb", File.open("config/routes.rb", &:read).gsub("end", "
+  get '/:inconu' => 'welcome#inconu'
+end
+"))
+
 end
 
-file 'app/views/welcome/index.html.erb', <<-CODE
-  <h1>Welcome to #{app_name}!<h1>
-CODE
+################################### ADMIN WELCOME ########################################
+
+if yes?("Do you want to create welcome page for admin? [yes/no]")
+
+  generate :controller, "admin/welcome dashboard"
+
+  file 'app/controllers/admin/welcome_controller.rb', <<-CODE
+class Admin::WelcomeController < Admin::ApplicationController
+  def dashboard
+    # if current_user.role == "superAdmin"
+    #   @users_count = User.all.count
+    #   @posts_count = Post.all.count
+    # else
+    #   @users_count= nil
+    #   @posts_count = current_user.posts.all.count
+    # end
+  end
+
+end
+  CODE
+
+  file 'app/views/admin/welcome/dashboard.html.erb', <<-CODE
+<h1>Welcome Admin to #{app_name.capitalize}!</h1>
+  CODE
+
+end
+
 
 if yes?("Do you want commit Welcome page? (yes/no)")
   git :add => "."
   git :commit => "-a -m 'Adding Welcome page'"
 end
+
+say <<-eos
+  ============================================================================
+  Your Welcome page is now available.
+eos
