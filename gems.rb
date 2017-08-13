c84eb4691b7f5c055d9412d3300f1d8c01587111
imidsac <imidsac@hotmail.fr>
2017-08-13 22:47:17 +0000

    Stup cancancan gem and more corrections and addition.

--- gems.rb
+++ gems.rb
@@ -2,12 +2,23 @@ gems_list = [
     "Devise",
     "Cancancan",
     "Bootstrap-sass",
+    "Bootstrap-social-rails",
     "Bootswatch-rails",
     "Font-awesome-sass",
+    "Bh",
     "Jquery-datatables-rails",
     "Bootstrap_form",
     "Paperclip",
     "Prawn",
+    "Globalize",
+    "Geocoder",
+    "Will_paginate",
+    "Simple_form",
+    "rails_layout",
+    "wysiwyg-rails",
+    "flot-rails",
+    "font-awesome-rails",
+    "morrisjs-rails",
     "Capistrano",
     "Whenever"
 ]
@@ -21,7 +32,7 @@ input = ask("Recommend me a name of gem to install?")
 case input.downcase.to_s
   when 'devise'
     ############################## Devise ##############################
-    if yes?("Would you like to install Devise? (yes/no)")
+    if yes?("Would you like to install Devise? [yes/no]")
 
       # Gems
       gem 'devise'
@@ -35,11 +46,11 @@ case input.downcase.to_s
       environment "config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }", env: 'development'
 
       model = "user" if model.blank?
-      generate "devise #{model.capitalize} username:string nom:string prenom:string role:string" if yes?("Do you want to Create table #{model.capitalize}? (yes/no)")
-      generate "devise:controllers #{model.pluralize}" if yes?("Do you want to create controller for #{model.pluralize}? (yes/no)")
-      generate "devise:views #{model.pluralize}" if yes?("Do you want to create views for #{model.pluralize}? (yes/no)")
+      generate "devise #{model.capitalize} username:string first_name:string last_name:string" if yes?("Do you want to Create table #{model.capitalize}? [yes/no]")
+      generate "devise:controllers #{model.pluralize}" if yes?("Do you want to create controller for #{model.pluralize}? [yes/no]")
+      generate "devise:views #{model.pluralize}" if yes?("Do you want to create views for #{model.pluralize}? [yes/no]")
 
-      if yes?("Do you want to edit routes for #{model.pluralize} ? (yes/no)")
+      if yes?("Do you want to edit routes for #{model.pluralize} ? [yes/no]")
         File.write("config/routes.rb", File.open("config/routes.rb", &:read).gsub("devise_for :#{model.pluralize}", "devise_for :#{model.pluralize},
            controllers: {
                registrations: '#{model.pluralize}/registrations',
@@ -57,8 +68,11 @@ case input.downcase.to_s
            }"))
       end
 
+      # Migrate db
+      rake "db:migrate" if yes?("Do you want to migrate db? [yes/no]")
+
       # Git
-      if yes?("Do you want commit Auth for #{model.pluralize}? (yes/no)")
+      if yes?("Do you want commit Auth for #{model.pluralize}? [yes/no]")
         git :add => "."
         git :commit => "-a -m 'Adding Authentication for #{model.pluralize}'"
       end
@@ -72,10 +86,10 @@ case input.downcase.to_s
   ############################## End Devise ##############################
   when 'cancancan'
     ############################## Cancancan ##############################
-    if yes?("Would you like to install Cancancan? (yes/no)")
+    if yes?("Would you like to install Cancancan? [yes/no]")
 
       # Gems
-      gem 'cancancan', '1.10'
+      gem 'cancancan'
 
       # install gems
       run 'bundle install'
@@ -83,8 +97,45 @@ case input.downcase.to_s
       # stup cancancan
       generate "cancan:ability"
 
+      if yes?("Do you want to Create table for Role? [yes/no]")
+        generate "model Role name:string"
+        generate "migration addRoleIdToUser role:references"
+      end
+
+      # Edit app/models/user.rb
+      # File.write("app/models/user.rb",
+      #            File.open("app/models/user.rb",
+      #                      &:read).gsub("class ApplicationController < ActionController::Base", "class Admin::ApplicationController < ActionController::Base"))
+
+      inject_into_file 'app/models/user.rb', :after => "class User < ApplicationRecord" do
+        "\nbelongs_to :role\nbefore_create :set_default_role\n\n\nprivate\ndef set_default_role\nself.role ||= Role.find_by_name('registered')\nend\n"
+      end
+
+      inject_into_file 'app/models/role.rb', :after => "class Role < ApplicationRecord" do
+        "\nhas_many :users\n"
+      end
+
+      append_file 'db/seeds.rb' do
+        <<-END
+['registered', 'banned', 'moderator', 'admin'].each do |role|
+  Role.find_or_create_by({name: role})
+end
+puts "===> Sample date have been set in DB  Roles!"
+
+User.delete_all
+User.create!([
+                 {id: -1000, username: "root", email: "imidsac@hotmail.fr", first_name: "SACKO", last_name: "IDRISS", role_id: 1, password: "walilahilhamdou"}
+             ])
+puts "===> Sample date have been set in DB  Users!"
+        END
+      end
+
+      # Migrate db and seed
+      rake "db:migrate" if yes?("Do you want to migrate db? [yes/no]")
+      rake "db:seed" if yes?("Do you want to seed data? [yes/no]")
+
       # Git
-      if yes?("Do you want commit Cancancan? (yes/no)")
+      if yes?("Do you want commit Cancancan? [yes/no]")
         git :add => "."
         git :commit => "-a -m 'Adding Cancancan'"
       end
@@ -98,11 +149,11 @@ case input.downcase.to_s
   ############################## End Cancancan ##############################
   when 'bootstrap-sass'
     ############################## Bootstrap-sass ##############################
-    if yes?("Would you like to install Bootstrap-sass? (yes/no)")
+    if yes?("Would you like to install Bootstrap-sass? [yes/no]")
 
       # Gems
       gem 'bootstrap-sass', '~> 3.3.6'
-      gem 'jquery-rails' if yes?("Do you want to add jquery-rails gem to Gemfile? (yes/no)")
+      gem 'jquery-rails' if yes?("Do you want to add jquery-rails gem to Gemfile? [yes/no]")
 
       # install gems
       run 'bundle install'
@@ -122,8 +173,15 @@ case input.downcase.to_s
 //= require bootstrap-sprockets
       END
 
+      if yes?("Would you like to install Bootstrap-sass for admin area? [yes/no]")
+        # Setup
+        run 'cp app/assets/stylesheets/application.scss app/assets/stylesheets/admin/application.scss'
+        run 'rm app/assets/stylesheets/admin/application.css'
+        run 'cp app/assets/javascripts/application.js app/assets/javascripts/admin/application.js'
+      end
+
       # Git
-      if yes?("Do you want commit Bootstrap-sass? (yes/no)")
+      if yes?("Do you want commit Bootstrap-sass? [yes/no]")
         git :add => "."
         git :commit => "-a -m 'Adding Bootstrap-sass'"
       end
@@ -137,7 +195,7 @@ case input.downcase.to_s
   ############################## End Bootstrap-sass ##############################
   when 'paperclip'
     ############################## Paperclip ##############################
-    if yes?("Would you like to install Paperclip? (yes/no)")
+    if yes?("Would you like to install Paperclip? [yes/no]")
 
       # Gems
       gem 'paperclip', '~> 5.0.0'
@@ -148,7 +206,7 @@ case input.downcase.to_s
       # Setup
 
       # Git
-      if yes?("Do you want commit Paperclip? (yes/no)")
+      if yes?("Do you want commit Paperclip? [yes/no]")
         git :add => "."
         git :commit => "-a -m 'Adding Paperclip'"
       end
@@ -162,7 +220,7 @@ case input.downcase.to_s
   ############################## End Paperclip ##############################
   when 'prawn'
     ############################## Prawn ##############################
-    if yes?("Would you like to install Prawn? (yes/no)")
+    if yes?("Would you like to install Prawn? [yes/no]")
 
       # Gems
       gem 'prawn', '~> 2.1.0'
@@ -179,7 +237,7 @@ case input.downcase.to_s
       end
 
       # Git
-      if yes?("Do you want commit Prawn? (yes/no)")
+      if yes?("Do you want commit Prawn? [yes/no]")
         git :add => "."
         git :commit => "-a -m 'Adding Prawn'"
       end
@@ -193,7 +251,7 @@ case input.downcase.to_s
   ############################## End Prawn ##############################
   when 'capistrano'
     ############################## Capistrano ##############################
-    if yes?("Would you like to install Capistrano? (yes/no)")
+    if yes?("Would you like to install Capistrano? [yes/no]")
 
       # Gems
       gem 'capistrano', '~> 3.4.0'
@@ -260,7 +318,7 @@ set :deploy_to, "/var/www/#{app_name}/production"
       END
 
       # Git
-      if yes?("Do you want commit Capistrano? (yes/no)")
+      if yes?("Do you want commit Capistrano? [yes/no]")
         git :add => "."
         git :commit => "-a -m 'Adding Capistrano'"
       end
@@ -274,7 +332,7 @@ set :deploy_to, "/var/www/#{app_name}/production"
   ############################## End Capistrano ##############################
   when 'whenever'
     ############################## Whenever ##############################
-    if yes?("Would you like to install Whenever? (yes/no)")
+    if yes?("Would you like to install Whenever? [yes/no]")
 
       # Gems
       gem 'whenever', :require => false
@@ -286,7 +344,7 @@ set :deploy_to, "/var/www/#{app_name}/production"
       run 'wheneverize .'
 
       # Git
-      if yes?("Do you want commit Whenever? (yes/no)")
+      if yes?("Do you want commit Whenever? [yes/no]")
         git :add => "."
         git :commit => "-a -m 'Adding Whenever gem'"
       end
@@ -300,7 +358,7 @@ set :deploy_to, "/var/www/#{app_name}/production"
   ############################## End Whenever ##############################
   when 'font-awesome-sass'
     ############################## font-awesome-sass ##############################
-    if yes?("Would you like to install font-awesome-sass? (yes/no)")
+    if yes?("Would you like to install font-awesome-sass? [yes/no]")
 
       # Gems
       gem 'font-awesome-sass', '~> 4.7.0'
@@ -311,13 +369,27 @@ set :deploy_to, "/var/www/#{app_name}/production"
       # Setup
 
       # Edit app/assets/stylesheets/application.scss
-      file 'app/assets/stylesheets/application.scss', <<-END
+      append_file 'app/assets/stylesheets/application.scss' do
+        <<-END
+      
 @import "font-awesome-sprockets";
 @import "font-awesome";
-      END
+        END
+      end
+
+      if yes?("Would you like to install font-awesome-sass for admin area? [yes/no]")
+        # Edit app/assets/stylesheets/admin/application.scss
+        append_file 'app/assets/stylesheets/admin/application.scss' do
+          <<-END
+      
+@import "font-awesome-sprockets";
+@import "font-awesome";
+          END
+        end
+      end
 
       # Git
-      if yes?("Do you want commit font-awesome-sass? (yes/no)")
+      if yes?("Do you want commit font-awesome-sass? [yes/no]")
         git :add => "."
         git :commit => "-a -m 'Adding font-awesome-sass gem'"
       end
@@ -331,7 +403,7 @@ set :deploy_to, "/var/www/#{app_name}/production"
   ############################## End font-awesome-sass ##############################
   when 'jquery-datatables-rails'
     ############################## jquery-datatables-rails ##############################
-    if yes?("Would you like to install jquery-datatables-rails? (yes/no)")
+    if yes?("Would you like to install jquery-datatables-rails? [yes/no]")
 
       # Gems
       gem 'jquery-datatables-rails', '~> 3.4.0'
@@ -366,7 +438,7 @@ set :deploy_to, "/var/www/#{app_name}/production"
       end
 
       # app/datatables
-      if yes?("Do you want to create app/datatables? (yes/no)")
+      if yes?("Do you want to create app/datatables? [yes/no]")
         unless File.exists?("app/datatables")
           Dir.mkdir("app/datatables")
           Dir.mkdir("app/datatables/ExempleUser")
@@ -507,7 +579,7 @@ $(document).on('turbolinks:load', function(){
 
 
       # Git
-      if yes?("Do you want commit jquery-datatables-rails? (yes/no)")
+      if yes?("Do you want commit jquery-datatables-rails? [yes/no]")
         git :add => "."
         git :commit => "-a -m 'Adding jquery-datatables-rails gem'"
       end
@@ -521,7 +593,7 @@ $(document).on('turbolinks:load', function(){
   ############################## End jquery-datatables-rails ##############################
   when 'bootstrap_form'
     ############################## bootstrap_form ##############################
-    if yes?("Would you like to install rails-bootstrap_form? (yes/no)")
+    if yes?("Would you like to install rails-bootstrap_form? [yes/no]")
 
       # Gems
       gem 'bootstrap_form'
@@ -538,7 +610,7 @@ $(document).on('turbolinks:load', function(){
       end
 
       # Git
-      if yes?("Do you want commit bootstrap_form? (yes/no)")
+      if yes?("Do you want commit bootstrap_form? [yes/no]")
         git :add => "."
         git :commit => "-a -m 'Adding bootstrap_form gem'"
       end
@@ -552,7 +624,7 @@ $(document).on('turbolinks:load', function(){
   ############################## End bootstrap_form ##############################
   when 'bootswatch-rails'
     ############################## bootswatch-rails ##############################
-    if yes?("Would you like to install rails-bootstrap_form? (yes/no)")
+    if yes?("Would you like to install rails-bootstrap_form? [yes/no]")
 
       # Gems
       gem 'bootswatch-rails'
@@ -586,7 +658,7 @@ body { padding-top: 60px; }
       end
 
       # Git
-      if yes?("Do you want commit bootswatch-rails? (yes/no)")
+      if yes?("Do you want commit bootswatch-rails? [yes/no]")
         git :add => "."
         git :commit => "-a -m 'Adding bootswatch-rails gem'"
       end
@@ -598,6 +670,200 @@ body { padding-top: 60px; }
 
     end
   ############################## End bootswatch-rails ##############################
+
+  when 'globalize'
+    ############################## globalize ##############################
+    if yes?("Would you like to install globalize? [yes/no]")
+
+      # Gems
+      # gem 'globalize'
+
+      # install gems
+      # run 'bundle install'
+
+      # Stup
+
+      # Git
+      # if yes?("Do you want commit globalize? [yes/no]")
+      # git :add => "."
+      # git :commit => "-a -m 'Adding globalize gem'"
+      # end
+
+      say <<-eos
+  ============================================================================
+  Your globalize is now available.
+      eos
+
+    end
+  ############################## globalize ##############################
+
+  when 'geocoder'
+    ############################## geocoder ##############################
+    if yes?("Would you like to install geocoder? [yes/no]")
+
+      # Gems
+      # gem 'geocoder'
+
+      # install gems
+      # run 'bundle install'
+
+      # Stup
+
+      # Git
+      # if yes?("Do you want commit geocoder? [yes/no]")
+      # git :add => "."
+      # git :commit => "-a -m 'Adding geocoder gem'"
+      # end
+
+      say <<-eos
+  ============================================================================
+  Your geocoder is now available.
+      eos
+
+    end
+  ############################## geocoder ##############################
+
+  when 'will_paginate'
+    ############################## will_paginate ##############################
+    if yes?("Would you like to install will_paginate? [yes/no]")
+
+      # Gems
+      # gem 'will_paginate'
+
+      # install gems
+      # run 'bundle install'
+
+      # Stup
+
+      # Git
+      # if yes?("Do you want commit will_paginate? [yes/no]")
+      # git :add => "."
+      # git :commit => "-a -m 'Adding will_paginate gem'"
+      # end
+
+      say <<-eos
+  ============================================================================
+  Your will_paginate is now available.
+      eos
+
+    end
+  ############################## will_paginate ##############################
+
+  when 'bh'
+    ############################## Bh ##############################
+    if yes?("Would you like to install Bh? [yes/no]")
+
+      # Gems
+      # gem 'bh'
+
+      # install gems
+      # run 'bundle install'
+
+      # Stup
+
+      # Git
+      # if yes?("Do you want commit Bh? [yes/no]")
+      # git :add => "."
+      # git :commit => "-a -m 'Adding Bh gem'"
+      # end
+
+      say <<-eos
+  ============================================================================
+  Your Bh is now available.
+      eos
+
+    end
+  ############################## Bh ##############################
+
+  when 'simple_form'
+    ############################## Simple_form ##############################
+    if yes?("Would you like to install Simple_form? [yes/no]")
+
+      # Gems
+      # gem 'simple_form'
+
+      # install gems
+      # run 'bundle install'
+
+      # Stup
+      # generate "rails generate simple_form:install --bootstrap"
+      # generate "rails generate simple_form:install --foundation"
+
+      # Git
+      # if yes?("Do you want commit Simple_form? [yes/no]")
+      #   git :add => "."
+      #   git :commit => "-a -m 'Adding Simple_form gem'"
+      # end
+
+      say <<-eos
+  ============================================================================
+  Your Simple_form is now available.
+      eos
+
+    end
+  ############################## Simple_form ##############################
+
+  when 'rails_layout'
+    ############################## rails_layout ##############################
+    if yes?("Would you like to install rails_layout? [yes/no]")
+
+      # Gems
+      # gem 'rails_layout', :group => [:development]
+
+      # install gems
+      # run 'bundle install'
+
+      # Stup
+      # generate "layout:install bootstrap4"
+      # generate "layout:install bootstrap3"
+
+      # Git
+      # if yes?("Do you want commit rails_layout? [yes/no]")
+      #   git :add => "."
+      #   git :commit => "-a -m 'Adding rails_layout gem'"
+      # end
+
+      say <<-eos
+  ============================================================================
+  Your rails_layout is now available.
+      eos
+
+    end
+  ############################## rails_layout ##############################
+
+  when 'wysiwyg-rails'
+    ############################## wysiwyg-rails ##############################
+    if yes?("Would you like to install wysiwyg-rails? [yes/no]")
+
+      # Gems
+      # gem "wysiwyg-rails"
+
+      # install gems
+      # run 'bundle install'
+
+      # Stup
+      #       append_file 'app/assets/stylesheets/application.scss' do
+      #         <<-EOF
+      # @import "froala_editor.min.css";
+      # @import "froala_style.min.css";
+      # @import "themes/dark.min.css";
+      #         EOF
+      #       end
+
+      # Git
+      # if yes?("Do you want commit rails_layout? [yes/no]")
+      #   git :add => "."
+      #   git :commit => "-a -m 'Adding rails_layout gem'"
+      # end
+
+      say <<-eos
+  ============================================================================
+  Your wysiwyg-rails is now available.
+      eos
+
+    end
+  ############################## wysiwyg-rails ##############################
+
   else
     say <<-eos
 
