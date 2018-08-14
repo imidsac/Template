cf8de6c2e9f5acc1f0308c1f91a3b434ff6b75dc
imidsac <imidsac@hotmail.fr>
2018-08-14 20:05:14 +0000

    Update mali.rb

--- mali.rb
+++ mali.rb
@@ -1,3 +1,20 @@
+if yes?("Do you want to create Scaffold for Country, Region, cercle, commune and quartier ? (yes/no)")
+  generate "scaffold", "country country_name:string country_name_short:string position:integer code:string latitude:float longitude:float"
+  generate "scaffold", "region country:references region_name:string region_name_short:string position:integer code:string latitude:float longitude:float"
+  generate "scaffold", "cercle region:references cercle_name:string cercle_name_short:string position:integer code:string latitude:float longitude:float"
+  generate "scaffold", "commune cercle:references commune_name:string commune_name_short:string position:integer code:string latitude:float longitude:float"
+  generate "scaffold", "quartier commune:references quartier_name:string quartier_name_short:string position:integer code:string latitude:float longitude:float"
+
+  generate "scaffold", "agency quartier:references agency_name:string agency_name_short:string position:integer code:string latitude:float longitude:float"
+
+  rails_command "db:migrate" if yes?("Do you want to migrate regions, cercles, communes and quartiers ? (yes/no)")
+
+  if yes?("Do you want commit? (yes/no)")
+    git :add => "."
+    git :commit => "-a -m 'Adding scaffold regions, cercles, communes and quartiers'"
+  end
+end
+
 if yes?("Do you want to create Scaffold for Religion and Ethnic ? (yes/no)")
   generate "scaffold", "religion religion_name"
   generate "scaffold", "ethnic ethnic_name"
@@ -10,22 +27,28 @@ if yes?("Do you want to create Scaffold for Religion and Ethnic ? (yes/no)")
   end
 end
 
-if yes?("Do you want to create Scaffold for Region, cercle, commune and quartier ? (yes/no)")
-  generate "scaffold", "region region_name:string region_name_short:string position:integer code:string latitude:float longitude:float"
-  generate "scaffold", "cercle region:references cercle_name:string cercle_name_short:string position:integer code:string latitude:float longitude:float"
-  generate "scaffold", "commune cercle:references commune_name:string commune_name_short:string position:integer code:string latitude:float longitude:float"
-  generate "scaffold", "quartier commune:references quartier_name:string quartier_name_short:string position:integer code:string latitude:float longitude:float"
+if yes?("Do you want to create Scaffold for Companies ? (yes/no)")
+  generate "scaffold", "company quartier:references name contacts:jsonb address settings:jsonb"
+  rails_command "db:migrate" if yes?("Do you want to migrate Companies ? (yes/no)")
 
-  rails_command "db:migrate" if yes?("Do you want to migrate regions, cercles, communes and quartiers ? (yes/no)")
-
-  if yes?("Do you want commit? (yes/no)")
+  if yes?("Do you want commit Companies ? (yes/no)")
     git :add => "."
-    git :commit => "-a -m 'Adding scaffold regions, cercles, communes and quartiers'"
+    git :commit => "-a -m 'Adding scaffold for Companies'"
+  end
+end
+
+if yes?("Do you want to create Scaffold for Licences ? (yes/no)")
+  generate "scaffold", "licence company:references start_date:datetime end_date:datetime "
+  rails_command "db:migrate" if yes?("Do you want to migrate Licences ? (yes/no)")
+
+  if yes?("Do you want commit Licences ? (yes/no)")
+    git :add => "."
+    git :commit => "-a -m 'Adding scaffold for Licences'"
   end
 end
 
 if yes?("Do you want to create Scaffold for Person (yes/no)")
-  generate "scaffold", "person first_name:string last_name:string middle_name:string sexe:string date_nai:datetime lieu_nai:string situation_family:string regime_matrimonial:string contacts:hstore address:string personId:hstore life:boolean"
+  generate "scaffold", "person religion:references ethnic:references first_name:string last_name:string middle_name:string sexe:string date_nai:datetime lieu_nai:string situation_family:string regime_matrimonial:string contacts:jsonb address:string personId:jsonb life:boolean"
 
   rails_command "db:migrate" if yes?("Do you want to migrate Person ? (yes/no)")
 
@@ -39,7 +62,7 @@ end
 if yes?("Do you want to do scaffold for Education ? (yes/no)")
 
   if yes?("Do you want to create Scaffold for cycle, level, filiere  ? (yes/no)")
-    generate "scaffold", "school quartier:references school_name contacts:hstore address:string academie:string reinscription:boolean latitude:float longitude:float"
+    generate "scaffold", "school quartier:references school_name contacts:jsonb address:string academie:string reinscription:boolean latitude:float longitude:float"
     generate "scaffold", "schoolYear annee_scolaire start_date:datetime end_date:datetime status:boolean"
     generate "scaffold", "grade grade_name grade_description"
 
@@ -58,7 +81,7 @@ if yes?("Do you want to do scaffold for Education ? (yes/no)")
 
 
     if yes?("Do you want to create Scaffold for Inscriptions ? (yes/no)")
-      generate "scaffold", "enrollment school:references person_student_id:integer type_enroll:string num:string num_enroll:string school_year:references date_enroll:datetime filiere:references grade:references options:hstore"
+      generate "scaffold", "enrollment school:references person_student_id:integer type_enroll:string num:string num_enroll:string school_year:references date_enroll:datetime filiere:references grade:references options:jsonb"
 
       rails_command "db:migrate" if yes?("Do you want to migrate Incriptions ? (yes/no)")
 
@@ -91,25 +114,10 @@ end
 ################## STORE #######################
 if yes?("Do you want to do scaffold for Store ? (yes/no)")
 
-end
-
-################## LABO PHOTO #######################
-if yes?("Do you want to do scaffold for Labo photo ? (yes/no)")
-
-  if yes?("Do you want to create Scaffold for Companies ? (yes/no)")
-    generate "scaffold", "company quartier:references name contacts:hstore address settings:hstore"
-    rails_command "db:migrate" if yes?("Do you want to migrate Companies ? (yes/no)")
-
-    if yes?("Do you want commit Companies ? (yes/no)")
-      git :add => "."
-      git :commit => "-a -m 'Adding scaffold for Companies'"
-    end
-  end
-
   if yes?("Do you want to create Scaffold for Products ? (yes/no)")
     generate "scaffold", "productType name"
     generate "scaffold", "productField product_type:references name field_type requied:boolean "
-    generate "scaffold", "product product_type:references name reference:string characteristics:hstore 'active:boolean:true'"
+    generate "scaffold", "product product_type:references name reference:string characteristics:jsonb 'active:boolean:true'"
     rails_command "db:migrate" if yes?("Do you want to migrate Products ? (yes/no)")
 
     if yes?("Do you want commit Products ? (yes/no)")
@@ -119,7 +127,7 @@ if yes?("Do you want to do scaffold for Labo photo ? (yes/no)")
   end
 
   if yes?("Do you want to create Scaffold for Orders ? (yes/no)")
-    generate "scaffold", "order 'subtotal:decimal{12,3}' 'tax:decimal{12,3}' 'shipping:decimal{12,3}' 'total:decimal{12,3}' order_status:references"
+    generate "scaffold", "order 'subtotal:decimal{12,3}' 'tax:decimal{12,3}' 'shipping:decimal{12,3}' 'total:decimal{12,3}' order_status:integer"
     generate "scaffold", "orderItem product:references order:references 'unit_price:decimal{12,3}' quantity:integer 'total_price:decimal{12,3}'"
     rails_command "db:migrate" if yes?("Do you want to migrate Orders ? (yes/no)")
 
@@ -136,16 +144,6 @@ if yes?("Do you want to do scaffold for Labo photo ? (yes/no)")
     if yes?("Do you want commit Payments ? (yes/no)")
       git :add => "."
       git :commit => "-a -m 'Adding scaffold for Payments'"
-    end
-  end
-
-  if yes?("Do you want to create Scaffold for Licences ? (yes/no)")
-    generate "scaffold", "licence company:references start "
-    rails_command "db:migrate" if yes?("Do you want to migrate Licences ? (yes/no)")
-
-    if yes?("Do you want commit Licences ? (yes/no)")
-      git :add => "."
-      git :commit => "-a -m 'Adding scaffold for Licences'"
     end
   end
 
